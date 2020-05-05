d2bbc62b2c80f7424fa8311b33342398a3a8f81b
phed-up <fed_up@hotmail.com>
2020-05-04 21:06:27 -0500

    add v4.1 for app store that includes URL to Gratipity app on app store.

--- TipDecider/CalculateViewController.swift
+++ TipDecider/CalculateViewController.swift
@@ -25,7 +25,7 @@ class CalculateViewController: UIViewController {
     var splitAmount = 1.0
     var dividedPayment = 0.0
     
-    var url = "https://phed-up.wixsite.com/fedapp"
+    var url = "https://apps.apple.com/us/app/gratipity/id1509368374?ls=1"
     
     
     
